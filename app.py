9aad4da54af60313b0723f7abfb5878e5d5a3f1d
sachin-h-s <89412649+sachin-h-s@users.noreply.github.com>
2022-12-29 17:21:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,17 +10,15 @@ st.markdown('''
 
 This is the **EDA App** created in Streamlit using the **pandas-profiling** library.
 
-**Credit:** App built in `Python` + `Streamlit` by [Chanin Nantasenamat](https://medium.com/@chanin.nantasenamat) (aka [Data Professor](http://youtube.com/dataprofessor))
-
 ---
 ''')
 
 # Upload CSV data
 with st.sidebar.header('1. Upload your CSV data'):
     uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
-    st.sidebar.markdown("""
-[Example CSV input file](https://raw.githubusercontent.com/dataprofessor/data/master/delaney_solubility_with_descriptors.csv)
-""")
+    #st.sidebar.markdown("""
+#[Example CSV input file](https://raw.githubusercontent.com/dataprofessor/data/master/delaney_solubility_with_descriptors.csv)
+#""")
 
 # Pandas Profiling Report
 if uploaded_file is not None:
@@ -37,15 +35,15 @@ if uploaded_file is not None:
     st_profile_report(pr)
 else:
     st.info('Awaiting for CSV file to be uploaded.')
-    if st.button('Press to use Example Dataset'):
+    #if st.button('Press to use Example Dataset'):
         # Example data
-        @st.cache
-        def load_data():
-            a = pd.DataFrame(
-                np.random.rand(100, 5),
-                columns=['a', 'b', 'c', 'd', 'e']
-            )
-            return a
+       # @st.cache
+       # def load_data():
+       #     a = pd.DataFrame(
+       #         np.random.rand(100, 5),
+        #        columns=['a', 'b', 'c', 'd', 'e']
+        #    )
+        #    return a
         df = load_data()
         pr = ProfileReport(df, explorative=True)
         st.header('**Input DataFrame**')
