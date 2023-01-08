93c97a9f85b2d59325775517a18307325583096d
sachin-h-s <89412649+sachin-h-s@users.noreply.github.com>
2023-01-08 18:13:36 +0530

    Update finalapp.py

--- finalapp.py
+++ finalapp.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import pandas_profiling
 import streamlit as st
 from pandas_profiling import ProfileReport
 from streamlit_pandas_profiling import st_profile_report
