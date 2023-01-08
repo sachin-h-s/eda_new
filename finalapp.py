341cbe1e81de56b56e713c873523710dcbb5c918
sachin-h-s <89412649+sachin-h-s@users.noreply.github.com>
2023-01-08 17:27:44 +0530

    Update finalapp.py

--- finalapp.py
+++ finalapp.py
@@ -49,5 +49,5 @@ if "df" in locals():
     if st.button("Generate report"):
         report = generate_report(df)
         st.write(report)
-if name == "main":
-	main()
+if name == "finalapp":
+	finalapp()
