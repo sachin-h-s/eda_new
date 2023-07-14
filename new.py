996f7555cb92c02f0866cd748951b2b97236a1b9
sachin-h-s <89412649+sachin-h-s@users.noreply.github.com>
2023-07-14 12:33:57 +0530

    Update new.py

--- new.py
+++ new.py
@@ -1,62 +1,47 @@
-import numpy as np
 import pandas as pd
-import pandas_profiling
 import streamlit as st
-from pandas_profiling import ProfileReport
-from streamlit_pandas_profiling import st_profile_report
+import pandas_profiling
 
-# Function to load data from a CSV file
-@st.cache
-def load_csv(file):
-    return pd.read_csv(file)
 
-# Function to load data from an XLSX file
-@st.cache
-def load_xlsx(file):
-    return pd.read_excel(file)
+def load_data(file):
+    file_extension = file.name.split(".")[-1]
 
-# Function to save data to a CSV file
-def save_csv(file, df):
-    df.to_csv(file, index=False)
+    if file_extension == "csv":
+        return pd.read_csv(file)
+    elif file_extension == "xlsx":
+        return pd.read_excel(file)
+    elif file_extension == "txt":
+        return pd.read_csv(file, delimiter="\t")
+    else:
+        return None
 
-# Function to load data from a SQL database
-@st.cache
-def load_sql(query, con):
-    return pd.read_sql(query, con)
 
-# Function to generate a pandas profiling report
-def generate_report(df):
-    return ProfileReport(df)
-
-# Main function
 def main():
     st.title("Exploratory Data Analysis App")
 
-    # Select data source
-    data_source = st.sidebar.selectbox("Select Data Source", ["CSV", "XLSX", "SQL"])
+    # Data loading options
+    data_source = st.sidebar.selectbox("Select Data Source", ["CSV", "XLSX", "TXT"])
 
-    # Load data
     if data_source == "CSV":
-        file = st.file_uploader("Upload CSV file", type="csv")
-        if file is not None:
-            df = load_csv(file)
+        file = st.file_uploader("Upload CSV file", type=["csv"])
     elif data_source == "XLSX":
-        file = st.file_uploader("Upload XLSX file", type="xlsx")
-        if file is not None:
-            df = load_xlsx(file)
-    elif data_source == "SQL":
-        query = st.text_input("SQL Query")
-        con = st.text_input("SQL Connection String")
-        if query and con:
-            df = load_sql(query, con)
+        file = st.file_uploader("Upload XLSX file", type=["xlsx"])
+    elif data_source == "TXT":
+        file = st.file_uploader("Upload TXT file", type=["txt"])
+    else:
+        file = None
 
-    # Generate report
-    if "df" in locals():
-        if st.button("Generate report"):
-            report = generate_report(df)
-            st_profile_report(report)
-            # st.write(report)
+    if file is not None:
+        df = load_data(file)
+
+        if df is not None:
+            st.subheader("Preview of the Dataset")
+            st.dataframe(df.head())
+
+            if st.button("Generate Report"):
+                profile = pandas_profiling.ProfileReport(df)
+                st_profile_report(profile)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
